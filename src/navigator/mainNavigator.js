85cbac826f9cf1c67c46abb08f949626ee56cc8c
namanmukund <namanmukund@gmail.com>
2020-05-14 16:33:46 +0530

    removing build error and also template codes

--- src/navigator/mainNavigator.js
+++ src/navigator/mainNavigator.js
@@ -1,15 +1,7 @@
 import {createAppContainer} from 'react-navigation';
 import {createStackNavigator} from 'react-navigation-stack';
-import {createDrawerNavigator} from 'react-navigation-drawer';
-
 import SplashScreen from '../features/SplashScreen';
-import SideMenu from './sideMenu';
 //@BlueprintImportInsertion
-import MessengerNavigator from '../features/Messenger/navigator';
-import TutorialNavigator from '../features/Tutorial/navigator';
-import MapsNavigator from '../features/Maps/navigator';
-import CalendarNavigator from '../features/Calendar/navigator';
-import CameraNavigator from '../features/Camera/navigator';
 import EmailAuthNavigator from '../features/EmailAuth/navigator';
 
 /**
@@ -21,11 +13,6 @@ const AppNavigator = {
     screen: SplashScreen,
   },
   //@BlueprintNavigationInsertion
-  // Messenger: { screen: MessengerNavigator },
-  // Tutorial: { screen: TutorialNavigator },
-  // Maps: { screen: MapsNavigator },
-  // Calendar: { screen: CalendarNavigator },
-  // Camera: { screen: CameraNavigator },
   EmailAuth: {screen: EmailAuthNavigator},
 
   /** new navigators can be added here */
