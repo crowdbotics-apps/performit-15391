6427c0580f1097be807ba3b2d4d2721fa658e86d
Hafiz Siddiq <mac@Macs-MacBook-Pro.local>
2020-06-01 16:56:39 +0500

    Model Adjusted

--- backend/posts/admin.py
+++ backend/posts/admin.py
@@ -6,4 +6,4 @@ from posts.models import Post
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
-    list_display = ['user', 'content']
+    list_display = ['user', 'content', 'created_at', 'updated_at']
