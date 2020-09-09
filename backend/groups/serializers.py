3355adc4570e6b24c81ccea1e6daaaf200137dbb
mac <no-reply@reachme.com>
2020-09-09 15:04:47 +0500

    Changes are added

--- backend/groups/serializers.py
+++ backend/groups/serializers.py
@@ -5,6 +5,14 @@ from users.serializers import CustomUserSerializer
 from posts.serializers import PostSerializer
 from groups.models import GroupPost, JoiningRequest, InviteUser
 
+class GroupMemberSerializer(serializers.ModelSerializer):
+    member_user = CustomUserSerializer(source="member", read_only=True)
+    # group_data = GroupSerializer(source="group", read_only=True)
+
+    class Meta:
+        model = GroupMembers
+        fields = '__all__'
+
 class GroupSerializer(serializers.ModelSerializer):
     group_owner = CustomUserSerializer(source="created_by", read_only=True)
     meta_data = serializers.SerializerMethodField()
@@ -14,8 +22,9 @@ class GroupSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
     def get_meta_data(self, obj):
-        group_members_count = GroupMembers.objects.filter(group=obj.id).count()
-        return {"group_member_count": group_members_count}
+        group_members = GroupMembers.objects.filter(group=obj.id)
+        serializer = GroupMemberSerializer(group_members, many=True)
+        return {"group_member_count": group_members.count(), "members": serializer.data}
 
 class GroupPostSerializer(serializers.ModelSerializer):
     post_data = PostSerializer(source="post", read_only=True)
@@ -34,14 +43,6 @@ class JoiningRequestSerializer(serializers.ModelSerializer):
         model = JoiningRequest
         fields = '__all__'
 
-
-class GroupMemberSerializer(serializers.ModelSerializer):
-    member_user = CustomUserSerializer(source="member", read_only=True)
-    group_data = GroupSerializer(source="group", read_only=True)
-
-    class Meta:
-        model = GroupMembers
-        fields = '__all__'
 
 
 class InviteUserSerializer(serializers.ModelSerializer):
