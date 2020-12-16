6ba7d5fa606d79f84ab868727c4f6b01150b6c00
Hafiz Siddiq <hafiz.siddiq86@gmail.com>
2020-12-16 17:11:03 +0500

    Worked on improving neaby users api

--- backend/nearby/urls.py
+++ backend/nearby/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from nearby.views import UpdateCurrentLocation, NearbyUsers
+from nearby.views import UpdateCurrentLocation, NearbyUsers, NearbyUsersAlternative
 app_name = "nearby"
 urlpatterns = [
     path("update-current-location/", view=UpdateCurrentLocation.as_view(), name="update-current-location"),
     path("users/", view=NearbyUsers.as_view(), name="nearby-users"),
+    path("users-alternative/", view=NearbyUsersAlternative.as_view(), name="nearby-users-alternative"),
 ]
